3ae331ffba2bb9267a143845b43e825f1e7976cb
zm382574 <zm382574@163.com>
2017-02-23 12:39:20 +0800

    Initial commit

---
+++ src/com/idea/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.idea.test;
+
+/**
+ * Created by Administrator on 2017/2/23 0023.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+         System.out.print("aaa");
+    }
+}
